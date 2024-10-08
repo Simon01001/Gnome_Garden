32aac27c33d69cc16146e6e1809325094d93c7de
Claudia Kwok <ksnclaudia@gmail.com>
2024-10-08 17:24:07 +0100

    update model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_08_135436) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_08_155641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,15 +31,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_08_135436) do
     t.string "image_path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "gnomes_owners", force: :cascade do |t|
-    t.bigint "gnome_id", null: false
     t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["gnome_id"], name: "index_gnomes_owners_on_gnome_id"
-    t.index ["user_id"], name: "index_gnomes_owners_on_user_id"
+    t.index ["user_id"], name: "index_gnomes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,6 +50,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_08_135436) do
 
   add_foreign_key "bookings", "gnomes"
   add_foreign_key "bookings", "users"
-  add_foreign_key "gnomes_owners", "gnomes"
-  add_foreign_key "gnomes_owners", "users"
+  add_foreign_key "gnomes", "users"
 end
