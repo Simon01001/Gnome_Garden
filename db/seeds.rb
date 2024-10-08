41ccf8e5e2a40c71a21fdd2ed62e552a44a9dadf
Damien Baubriau <dam.baubriau@gmail.com>
2024-10-08 15:56:38 +0100

    Update Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,20 +4,27 @@ Booking.destroy_all
 
 Gnome.destroy_all
 
-User.destroy_all
+puts "Creating gnomes..."
+
+Gnome.create!(name: "Gnome with spade", description: "A gnome with a spade", price: 9.99)
+
+Gnome.create!(name: "Gnome with hat", description: "A gnome with a big red hat", price: 10.99)
+
+Gnome.create!(name: "Gnome with fishing rod", description: "A gnome holding a fishing rod", price: 12.99)
+
+Gnome.create!(name: "Gnome reading book", description: "A gnome sitting and reading a book", price: 8.99)
+
+Gnome.create!(name: "Gnome with lantern", description: "A gnome holding a small lantern", price: 11.50)
+
+Gnome.create!(name: "Gnome with watering can", description: "A gnome with a watering can", price: 9.50)
+
+Gnome.create!(name: "Gnome sitting on mushroom", description: "A gnome sitting on a big mushroom", price: 13.99)
+
+Gnome.create!(name: "Gnome with wheelbarrow", description: "A gnome pushing a wheelbarrow", price: 10.75)
+
+Gnome.create!(name: "Gnome with shovel", description: "A gnome holding a small shovel", price: 9.00)
+
+Gnome.create!(name: "Gnome with flowers", description: "A gnome holding a bouquet of flowers", price: 12.50)
 
 
-
-puts "Creating user..."
-
-
-unless User.exists?(email: 'user1@example.com')
-  User.create!(
-    email: 'user1@example.com',
-    password: 'password123',
-    password_confirmation: 'password123'
-  )
-end
-
-
-puts "Seed created! #{User.all.count} user(s) created"
+puts "Seed created! #{Gnom.all.count} gnomes created."
