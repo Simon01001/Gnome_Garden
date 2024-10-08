3fab76dd9cf71b65462e7fd1d6a5cf35316422c0
Claudia Kwok <ksnclaudia@gmail.com>
2024-10-08 13:39:04 +0100

    add model validation

--- app/models/gnome.rb
+++ app/models/gnome.rb
@@ -1,3 +1,7 @@
 class Gnome < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
+  validates :name, :description, :price, :image_path, presence: true
+  validates :name, uniqueness: true
 end
