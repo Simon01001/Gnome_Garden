635c8315f28e56072d2308899fae57919377930e
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-10-09 11:44:13 +0100

    changed migration file add_user_to_gnomes

--- db/migrate/20241008155255_add_user_to_gnomes.rb
+++ db/migrate/20241008155255_add_user_to_gnomes.rb
@@ -1,5 +1,5 @@
 class AddUserToGnomes < ActiveRecord::Migration[7.1]
   def change
-    add_reference :gnomes, :user, null: false, foreign_key: true
+    add_reference :gnomes, :user, null: true, foreign_key: true
   end
 end
