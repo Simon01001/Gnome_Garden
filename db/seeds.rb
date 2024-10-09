59dafbca99dbb791efb55505450d594895872a99
Damien Baubriau <dam.baubriau@gmail.com>
2024-10-09 12:44:03 +0100

    Update Seed with bookings and images

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,25 +17,36 @@ users << User.create!(email: 'user3@example.com', password: 'password123')
 
 puts "Creating gnomes..."
 
-Gnome.create!(name: "Gnome with spade", description: "A gnome with a spade", price: 9.99, user: users.sample)
+gnomes = []
 
-Gnome.create!(name: "Gnome with hat", description: "A gnome with a big red hat", price: 10.99, user: users.sample)
+gnomes << Gnome.create!(name: "Gnome with spade", description: "A gnome with a spade", price: 9.99, user: users.sample, image_path: "app/assets/images/gnome1.jpeg")
 
-Gnome.create!(name: "Gnome with fishing rod", description: "A gnome holding a fishing rod", price: 12.99, user: users.sample)
+gnomes << Gnome.create!(name: "Gnome with hat", description: "A gnome with a big red hat", price: 10.99, user: users.sample, image_path: "app/assets/images/gnome2.jpeg")
 
-Gnome.create!(name: "Gnome reading book", description: "A gnome sitting and reading a book", price: 8.99, user: users.sample)
+gnomes << Gnome.create!(name: "Gnome with fishing rod", description: "A gnome holding a fishing rod", price: 12.99, user: users.sample, image_path: "app/assets/images/gnome3.jpeg")
 
-Gnome.create!(name: "Gnome with lantern", description: "A gnome holding a small lantern", price: 11.50, user: users.sample)
+gnomes << Gnome.create!(name: "Gnome reading book", description: "A gnome sitting and reading a book", price: 8.99, user: users.sample, image_path: "app/assets/images/gnome4.jpeg")
 
-Gnome.create!(name: "Gnome with watering can", description: "A gnome with a watering can", price: 9.50, user: users.sample)
+gnomes << Gnome.create!(name: "Gnome with lantern", description: "A gnome holding a small lantern", price: 11.50, user: users.sample, image_path: "app/assets/images/gnome5.jpeg")
 
-Gnome.create!(name: "Gnome sitting on mushroom", description: "A gnome sitting on a big mushroom", price: 13.99, user: users.sample)
+gnomes << Gnome.create!(name: "Gnome with watering can", description: "A gnome with a watering can", price: 9.50, user: users.sample, image_path: "app/assets/images/gnome6.jpeg")
 
-Gnome.create!(name: "Gnome with wheelbarrow", description: "A gnome pushing a wheelbarrow", price: 10.75, user: users.sample)
+gnomes << Gnome.create!(name: "Gnome sitting on mushroom", description: "A gnome sitting on a big mushroom", price: 13.99, user: users.sample, image_path: "app/assets/images/gnome7.jpeg")
 
-Gnome.create!(name: "Gnome with shovel", description: "A gnome holding a small shovel", price: 9.00, user: users.sample)
+gnomes << Gnome.create!(name: "Gnome with wheelbarrow", description: "A gnome pushing a wheelbarrow", price: 10.75, user: users.sample, image_path: "app/assets/images/gnome8.jpeg")
 
-Gnome.create!(name: "Gnome with flowers", description: "A gnome holding a bouquet of flowers", price: 12.50, user: users.sample)
+gnomes << Gnome.create!(name: "Gnome with shovel", description: "A gnome holding a small shovel", price: 9.00, user: users.sample, image_path: "app/assets/images/gnome9.jpeg")
 
+gnomes << Gnome.create!(name: "Gnome with flowers", description: "A gnome holding a bouquet of flowers", price: 12.50, user: users.sample, image_path: "app/assets/images/gnome10.jpeg")
 
-puts "Seed created! #{User.all.count} users created & #{Gnome.all.count} gnomes created."
+puts "Creating bookings..."
+
+bookings = []
+
+bookings << Booking.create!(booking_date: Date.today + 1, user: users.sample, gnome: gnomes.sample)
+bookings << Booking.create!(booking_date: Date.today + 2, user: users.sample, gnome: gnomes.sample)
+bookings << Booking.create!(booking_date: Date.today + 3, user: users.sample, gnome: gnomes.sample)
+bookings << Booking.create!(booking_date: Date.today + 4, user: users.sample, gnome: gnomes.sample)
+bookings << Booking.create!(booking_date: Date.today + 5, user: users.sample, gnome: gnomes.sample)
+
+puts "Seed created! #{User.all.count} users created & #{Gnome.all.count} gnomes created & #{Booking.all.count} bookings created."
