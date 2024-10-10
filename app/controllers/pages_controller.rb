8bc47d0ee7038dbb22f4bae4bc643eb924b26c51
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-10-10 12:35:53 +0100

    implemented functionality to allow a logged in User to view all of the bookings they have made specifically

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,9 @@ class PagesController < ApplicationController
   def my_profile
     @gnomes = Gnome.where(user:current_user)
   end
+
+  def mybookings
+    user = current_user
+    @bookings = user.bookings
+  end
 end
