a4564b83a30abc146c6c476092e95c03f76e22c8
szymon niedorezo <szymonniedorezo@gmail.com>
2024-10-10 13:08:18 +0100

    changed seed file images and added grid to index

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,25 +19,25 @@ puts "Creating gnomes..."
 
 gnomes = []
 
-gnomes << Gnome.create!(name: "Gnome with spade", description: "A gnome with a spade", price: 9.99, user: users.sample, image_path: "app/assets/images/gnome1.jpeg")
+gnomes << Gnome.create!(name: "Gnome with spade", description: "A gnome with a spade", price: 9.99, user: users.sample, image_path: "gnome1.jpeg")
 
-gnomes << Gnome.create!(name: "Gnome with hat", description: "A gnome with a big red hat", price: 10.99, user: users.sample, image_path: "app/assets/images/gnome2.jpeg")
+gnomes << Gnome.create!(name: "Gnome with hat", description: "A gnome with a big red hat", price: 10.99, user: users.sample, image_path: "gnome2.jpeg")
 
-gnomes << Gnome.create!(name: "Gnome with fishing rod", description: "A gnome holding a fishing rod", price: 12.99, user: users.sample, image_path: "app/assets/images/gnome3.jpeg")
+gnomes << Gnome.create!(name: "Gnome with fishing rod", description: "A gnome holding a fishing rod", price: 12.99, user: users.sample, image_path: "gnome3.jpeg")
 
-gnomes << Gnome.create!(name: "Gnome reading book", description: "A gnome sitting and reading a book", price: 8.99, user: users.sample, image_path: "app/assets/images/gnome4.jpeg")
+gnomes << Gnome.create!(name: "Gnome reading book", description: "A gnome sitting and reading a book", price: 8.99, user: users.sample, image_path: "gnome4.jpeg")
 
-gnomes << Gnome.create!(name: "Gnome with lantern", description: "A gnome holding a small lantern", price: 11.50, user: users.sample, image_path: "app/assets/images/gnome5.jpeg")
+gnomes << Gnome.create!(name: "Gnome with lantern", description: "A gnome holding a small lantern", price: 11.50, user: users.sample, image_path: "gnome5.jpeg")
 
-gnomes << Gnome.create!(name: "Gnome with watering can", description: "A gnome with a watering can", price: 9.50, user: users.sample, image_path: "app/assets/images/gnome6.jpeg")
+gnomes << Gnome.create!(name: "Gnome with watering can", description: "A gnome with a watering can", price: 9.50, user: users.sample, image_path: "gnome6.jpeg")
 
-gnomes << Gnome.create!(name: "Gnome sitting on mushroom", description: "A gnome sitting on a big mushroom", price: 13.99, user: users.sample, image_path: "app/assets/images/gnome7.jpeg")
+gnomes << Gnome.create!(name: "Gnome sitting on mushroom", description: "A gnome sitting on a big mushroom", price: 13.99, user: users.sample, image_path: "gnome7.jpeg")
 
-gnomes << Gnome.create!(name: "Gnome with wheelbarrow", description: "A gnome pushing a wheelbarrow", price: 10.75, user: users.sample, image_path: "app/assets/images/gnome8.jpeg")
+gnomes << Gnome.create!(name: "Gnome with wheelbarrow", description: "A gnome pushing a wheelbarrow", price: 10.75, user: users.sample, image_path: "gnome8.jpeg")
 
-gnomes << Gnome.create!(name: "Gnome with shovel", description: "A gnome holding a small shovel", price: 9.00, user: users.sample, image_path: "app/assets/images/gnome9.jpeg")
+gnomes << Gnome.create!(name: "Gnome with shovel", description: "A gnome holding a small shovel", price: 9.00, user: users.sample, image_path: "gnome9.jpeg")
 
-gnomes << Gnome.create!(name: "Gnome with flowers", description: "A gnome holding a bouquet of flowers", price: 12.50, user: users.sample, image_path: "app/assets/images/gnome10.jpeg")
+gnomes << Gnome.create!(name: "Gnome with flowers", description: "A gnome holding a bouquet of flowers", price: 12.50, user: users.sample, image_path: "gnome10.jpeg")
 
 puts "Creating bookings..."
 
