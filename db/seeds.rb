650652a58885b267a587cadd879716ed6e06242b
szymon niedorezo <szymonniedorezo@gmail.com>
2024-10-11 15:40:24 +0100

    changed prices

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,17 +27,17 @@ gnomes << Gnome.create!(name: "Gnome with fishing rod", description: "A gnome ho
 
 gnomes << Gnome.create!(name: "Gnome reading book", description: "A gnome sitting and reading a book", price: 8.99, user: users.sample, image_path: "gnome4.jpeg")
 
-gnomes << Gnome.create!(name: "Gnome with lantern", description: "A gnome holding a small lantern", price: 11.50, user: users.sample, image_path: "gnome5.jpeg")
+gnomes << Gnome.create!(name: "Gnome with lantern", description: "A gnome holding a small lantern", price: 11.59, user: users.sample, image_path: "gnome5.jpeg")
 
-gnomes << Gnome.create!(name: "Gnome with watering can", description: "A gnome with a watering can", price: 9.50, user: users.sample, image_path: "gnome6.jpeg")
+gnomes << Gnome.create!(name: "Gnome with watering can", description: "A gnome with a watering can", price: 9.59, user: users.sample, image_path: "gnome6.jpeg")
 
 gnomes << Gnome.create!(name: "Gnome sitting on mushroom", description: "A gnome sitting on a big mushroom", price: 13.99, user: users.sample, image_path: "gnome7.jpeg")
 
 gnomes << Gnome.create!(name: "Gnome with wheelbarrow", description: "A gnome pushing a wheelbarrow", price: 10.75, user: users.sample, image_path: "gnome8.jpeg")
 
-gnomes << Gnome.create!(name: "Gnome with shovel", description: "A gnome holding a small shovel", price: 9.00, user: users.sample, image_path: "gnome9.jpeg")
+gnomes << Gnome.create!(name: "Gnome with shovel", description: "A gnome holding a small shovel", price: 8.99, user: users.sample, image_path: "gnome9.jpeg")
 
-gnomes << Gnome.create!(name: "Gnome with flowers", description: "A gnome holding a bouquet of flowers", price: 12.50, user: users.sample, image_path: "gnome10.jpeg")
+gnomes << Gnome.create!(name: "Gnome with flowers", description: "A gnome holding a bouquet of flowers", price: 12.59, user: users.sample, image_path: "gnome10.jpeg")
 
 puts "Creating bookings..."
 
